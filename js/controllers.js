9a929d6a5677eb92474dc6fbd5278a1c7c490330
German <schacht.german@gmail.com>
2015-01-16 17:55:11 -0300

    Prueba de posicionamiendo de cosas interesantes

--- js/controllers.js
+++ js/controllers.js
@@ -25,6 +25,27 @@ valorameControllers.controller('ListadoController',['$scope', function($scope){
 	 	'stars': 5,
 	 	'date': '2015-01-10',
 	 	'comentator': 'Pepsi co'
+	},
+	{
+		'votated': 'Personal',
+		'comment': 'La verdad que estoy muy disconforme con la compañía. Deja mucho que desear. Sobretodo la conexión!!!',
+		'stars': 1,
+		'date': '2015-01-16',
+		'comentator': 'Fernando Luján'
+	},
+	{
+		'votated': 'Dell',
+	 	'comment': 'Realmente no lo recomiendo, tuve una y terminé quemándola. Ahora es un pisapapeles caro',
+	 	'stars': 2,
+	 	'date': '2015-01-14',
+	 	'comentator': 'Kasuki Takemura'
+	},
+	{
+		'votated': 'Coca Cola',
+	 	'comment': 'Altamente recomendada, sobretodo cuando hace calor',
+	 	'stars': 5,
+	 	'date': '2015-01-10',
+	 	'comentator': 'Pepsi co'
 	}
 	]
 }]);
