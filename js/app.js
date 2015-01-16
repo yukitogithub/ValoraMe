a20c9c816515148d303592e11fe369b96e58d494
German <schacht.german@gmail.com>
2015-01-16 13:06:16 -0300

    Testing partial views, implemented simple routing and changed Index name for a generic index one.

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var valorame = angular.module('valorame',[
+	'ngRoute',
+	'valorameControllers'
+	]);
+
+valorame.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider
+		.when('/index', {
+			templateUrl: 'partials/portada.html',
+			controller: 'PortadaController'
+		})
+		.when('/listado', {
+			templateUrl: 'partials/listado.html',
+			controller: 'PortadaController'
+		})
+		.otherwise({
+			redirectTo: '/index'
+		});
+	}]);
---
+++ js/controllers.js
@@ -0,0 +1,5 @@
+var valorameControllers = angular.module('valorameControllers', []);
+
+valorameControllers.controller('PortadaController',['$scope', function($scope){
+	$scope.casa = 'perro';
+}]);
