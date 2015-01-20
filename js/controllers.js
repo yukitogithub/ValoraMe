a0854b1d846aa0421deaf975892efb3428761166
Fernando Luján <lujanfernandogabriel@gmail.com>
2015-01-20 17:25:00 -0300

    Functionality for adding a new product service

--- js/controllers.js
+++ js/controllers.js
@@ -49,6 +49,26 @@ valorameControllers.controller('ListadoController',['$scope', function($scope){
 	}
 	];
 
+	$scope.products = [{
+		'name': 'Personal',
+		'description': 'Una empresa de telefonía',
+		'date': '2015-01-19',
+		'owner': 'Alguien'
+	},
+	{
+		'name': 'Dell',
+		'description': 'Una empresa de informática',
+		'date': '2014-12-14',
+		'owner': 'Alguien 2'
+	},
+	{
+		'name': 'Coca Cola',
+		'description': 'Una empresa de refrescos',
+		'date': '2013-10-10',
+		'owner': 'Alguien 3'
+	}
+	]
+
 	$scope.agregarComentario = function() {
 		var nuevoComentario = {
 			'votated' : $scope.votated,
@@ -60,12 +80,21 @@ valorameControllers.controller('ListadoController',['$scope', function($scope){
 
 		$scope.data.push(nuevoComentario);
 
-		$scope.votated = '';
-		$scope.comment = '';
-		$scope.stars = '';
-		$scope.date = '';
-		$scope.comentator = '';
+		$scope.limpiarComentario();
 	};
+
+	$scope.agregarProducto = function(){
+		var nuevoProducto = {
+			'name': $scope.votated,
+			'description': $scope.description,
+			'date': $scope.productDate,
+			'owner': $scope.comentator
+		}
+
+		$scope.products.push(nuevoProducto);
+
+		$scope.volveraComentario();
+	}
 
 	$scope.criteria = "Any";
 
@@ -79,12 +108,26 @@ valorameControllers.controller('ListadoController',['$scope', function($scope){
 
 	$scope.select = false;
 	$scope.selected = function(){
-		$scope.votated = $(this)[0].item.votated;
+		$scope.votated = $(this)[0].item.name;
 		$scope.select = true;
  	}
 
  	$scope.crearProducto = function(){
  		$("#comentarioForm").hide();
  		$("#productoForm").show();
+ 	}
+
+ 	$scope.limpiarProducto = function() {
+		$scope.votated = '';
+		$scope.description = '';
+		$scope.productDate = '';
+		$scope.comentator = '';
+	}
+
+	$scope.volveraComentario = function(){
+ 		$("#productoForm").hide();
+ 		$("#comentarioForm").show();
+ 		$scope.description = '';
+		$scope.productDate = '';
  	}
 }]);
